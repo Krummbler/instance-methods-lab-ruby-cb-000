520bec4857a1d9b4b62361204e88ce87cc14dd4b
Brian Krumm <bjkrumm1@gmail.com>
2018-03-14 21:19:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,4 @@
 class Dog
-  def bark
-    puts "Woof!"
-  end
-  def sit
-    puts "The Dog is sitting"
-  end
-end
-
-class Person
-  def talk
-    puts "Hello World!"
-  end
-  def walk
-    puts "The Person is walking"
-  end
+  def name=(dog_name)
+    this_dogs_name = dog_name
 end
